f3f53150c780e206ff77d90e207aba3770c8d773
Trisout <81978700+trisout78@users.noreply.github.com>
2024-03-11 22:23:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const Discord = require('discord.js-selfbot-v13');
 const client = new Discord.Client();
+const fs = require('fs');
+
+let blockedUserIDs = [];
+
+function loadBlockedUsers() {
+  try {
+    const data = fs.readFileSync('blocked_users.json', 'utf8');
+    blockedUserIDs = JSON.parse(data);
+  } catch (err) {
+    // If the file doesn't exist, create an empty array
+    blockedUserIDs = [];
+  }
+}
+
+function saveBlockedUsers() {
+  fs.writeFileSync('blocked_users.json', JSON.stringify(blockedUserIDs), 'utf8');
+}
+
+loadBlockedUsers();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    status: 'dnd',
+  });
 });
 
 client.on('message', (message) => {
-  if (message.content.endsWith('Quoi') && message.author.id !== client.user.id) {
+  if (blockedUserIDs.includes(message.author.id)) {
+    return; // Do nothing if the user is blocked
+  }
+  else if (message.content.endsWith('Quoi') && message.author.id !== client.user.id) {
     message.channel.sendTyping();
     setTimeout(function() {
         message.reply('feur');
@@ -161,21 +186,32 @@ client.on('message', (message) => {
     setTimeout(function() {
         message.reply('den');
     }, 2000);
-  }   
+  }
+  else if (message.content.startsWith('feur!arretedemefeurstp')) {
+    // Add user ID to the blocked list
+    blockedUserIDs.push(message.author.id);
+    saveBlockedUsers(); // Save the updated list to JSON
+    message.reply('✅');
+  }
+  else if (message.content.startsWith('feur!join')) {
+    const args = message.content.slice('feur!join'.length).trim().split(' ');
+    const inviteCode = args[0];
+
+    try {
+      client.acceptInvite(inviteCode, { bypassOnboarding: true, bypassVerify: true });
+      message.reply('✅');
+    } catch (error) {
+      if (error.statusCode === 500) {
+        message.reply(':x: Oups, comment résoudre un captcha? ')
+      } else {
+        console.error(error);
+      }
+    }
+  }
 });
 
 process.on('uncaughtException', function (err) {
   console.log(err);
-});
-
-client.on('message', (message) => {
-  if (message.content.startsWith('feur!join')) {
-    const args = message.content.slice('feur!join'.length).trim().split(' ');
-    const inviteCode = args[0];
-
-    client.acceptInvite(inviteCode, { bypassOnboarding: true, bypassVerify: true });
-    message.reply('✅');
-  }
 });
 
 client.login('token');
